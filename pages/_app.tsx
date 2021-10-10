65e4e34da7c5ff6801c3e76f509fc5a5a79c1037
Akshay Ganeshen <me@aganeshen.com>
2021-10-10 16:04:47 -0400

    feat(theme): customize fonts and enable dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,20 @@ import type { AppProps } from "next/app";
 
 import "../styles/globals.scss";
 
-const theme = responsiveFontSizes(createTheme({}));
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+    typography: {
+      fontFamily: "sans-serif",
+      h1: { fontSize: "6rem", fontWeight: 600 },
+      h2: { fontSize: "2rem", fontWeight: 400 },
+      h3: { fontSize: "1.2rem", fontWeight: 300 },
+      button: { textTransform: "unset" },
+    },
+  })
+);
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
