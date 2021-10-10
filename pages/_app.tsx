a123488cbf5d59c7559bcc888bc34f6848fe5f8a
Akshay Ganeshen <me@aganeshen.com>
2021-10-10 16:03:49 -0400

    feat: add mui css baseline and theme to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const theme = responsiveFontSizes(createTheme({}));
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
 export default MyApp;
